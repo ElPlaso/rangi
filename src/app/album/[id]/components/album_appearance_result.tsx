57ffbc2e39ff31370016236da892e4f53beac727
Moo Plah Soe Kusay <kusaymoo@ecs.vuw.ac.nz>
2023-03-05 13:35:15 +1300

    album_page: update ui.

---
+++ src/app/album/[id]/components/album_appearance_result.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Inter } from "@next/font/google";
+import styles from "@/app/styles/page.module.css";
+import React, { useState } from "react";
+import SearchResult from "@/app/search/[id]/components/search_result";
+import Sample from "@/app/models/sample";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+async function getSamples(id: String) {
+  let headers = new Headers();
+  headers.append(
+    "X-RapidAPI-Key",
+    process.env.NEXT_PUBLIC_RAPID_API_KEY as string
+  );
+  headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
+  const options: RequestInit = {
+    method: "GET",
+    headers: headers,
+    cache: "no-store",
+  };
+
+  return fetch(
+    "https://genius-song-lyrics1.p.rapidapi.com/song/details/?id=" + id,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => data["song"]["song_relationships"]["0"]["songs"])
+    .catch((err) => console.error(err));
+}
+
+export default function AlbumAppearanceResult({ songData }: any) {
+  const { song } = songData || {};
+
+  const [samples, setSamples] = useState([] as any[]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [loaded, setLoaded] = useState(false);
+
+  const [hidden, setHidden] = useState(false);
+
+  const handleClick = async () => {
+    if (!loaded) {
+      setLoading(true);
+      let data = await getSamples(song.id);
+      setSamples(data);
+      setLoading(false);
+      setLoaded(true);
+    } else {
+      setHidden(!hidden);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.card} onClick={handleClick}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <span>
+              <h3 className={inter.className}>{song["title"]}</h3>
+              <p className={inter.className}>{song["artist_names"]}</p>
+            </span>
+          </div>
+          <div>
+            <span>
+              <Link href={`/samples/${song.id}`} className={inter.className}>
+                <p className={styles.card}>More -{">"}</p>
+              </Link>
+            </span>
+          </div>
+        </div>
+        <div className={styles.grid}>
+          {loading ? (
+            <p className={inter.className} style={{ margin: "2rem" }}>
+              Loading . . .
+            </p>
+          ) : (
+            loaded &&
+            !hidden &&
+            (samples?.length > 0 ? (
+              samples?.map((sample) => {
+                return (
+                  <SearchResult
+                    key={sample.id}
+                    type="samples"
+                    result={
+                      new Sample(
+                        sample.id,
+                        sample["title"],
+                        sample["artist_names"],
+                        sample["release_date_components"]
+                          ? sample["release_date_components"]["year"]
+                          : "-",
+                        sample["song_art_image_thumbnail_url"]
+                      )
+                    }
+                  />
+                );
+              })
+            ) : (
+              <p className={inter.className} style={{ margin: "2rem" }}>
+                No samples found.
+              </p>
+            ))
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -1,215 +1,115 @@
-"use client";
-
 import { Inter } from "@next/font/google";
-import AlbumAppearance from "@/app/models/album_appearance";
-import AlbumAppearanceCard from "./components/album_appearance_card";
 import Image from "next/image";
 import styles from "@/app/styles/page.module.css";
-import React, { useState, useEffect } from "react";
-import Sample from "@/app/models/sample";
-import "@/app/styles/scrollbar.css";
 import Link from "next/link";
-import SampleResult from "@/app/samples/[id]/components/sample_result";
+import AlbumAppearanceResult from "./components/album_appearance_result";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function AlbumPage({ params }: any) {
-  const [songs, setSongs] = useState([] as any[]);
+async function getAlbumData(id: String) {
+  let headers = new Headers();
+  headers.append(
+    "X-RapidAPI-Key",
+    process.env.NEXT_PUBLIC_RAPID_API_KEY as string
+  );
+  headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
 
-  const [albumData, setAlbumData] = useState(null as any);
-
-  const [currentSongID, setCurrentSongID] = useState(null as any);
-
-  const [currentSongData, setCurrentSongData] = useState(null as any);
-
-  const [currentSamples, setCurrentSamples] = useState([] as any[]);
-
-  useEffect(() => {
-    let headers = new Headers();
-    headers.append(
-      "X-RapidAPI-Key",
-      process.env.NEXT_PUBLIC_RAPID_API_KEY as string
-    );
-    headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
-
-    const options: RequestInit = {
-      method: "GET",
-      headers: headers,
-      cache: "no-store",
-    };
-
-    fetch(
-      "https://genius-song-lyrics1.p.rapidapi.com/album/appearances/?id=" +
-        params.id +
-        "&page=1",
-      options
-    )
-      .then((response) => response.json())
-      .then((data) => setSongs(data["album_appearances"]))
-      .catch((err) => console.error(err));
-
-    fetch(
-      "https://genius-song-lyrics1.p.rapidapi.com/album/details/?id=" +
-        params.id,
-      options
-    )
-      .then((response) => response.json())
-      .then((data) => setAlbumData(data["album"]))
-      .catch((err) => console.error(err));
-  }, [params.id]);
-
-  const handleSongSelect = (index: number) => {
-    setCurrentSongID(songs[index]["song"].id);
+  const options: RequestInit = {
+    method: "GET",
+    headers: headers,
+    cache: "no-store",
   };
 
-  useEffect(() => {
-    let headers = new Headers();
-    headers.append(
-      "X-RapidAPI-Key",
-      process.env.NEXT_PUBLIC_RAPID_API_KEY as string
-    );
-    headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
+  return fetch(
+    "https://genius-song-lyrics1.p.rapidapi.com/album/details/?id=" + id,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => data["album"])
+    .catch((err) => console.error(err));
+}
 
-    const options: RequestInit = {
-      method: "GET",
-      headers: headers,
-      cache: "no-store",
-    };
+async function getAlbumAppearances(id: String) {
+  let headers = new Headers();
+  headers.append(
+    "X-RapidAPI-Key",
+    process.env.NEXT_PUBLIC_RAPID_API_KEY as string
+  );
+  headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
 
-    fetch(
-      "https://genius-song-lyrics1.p.rapidapi.com/song/details/?id=" +
-        currentSongID,
-      options
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setCurrentSamples(data["song"]["song_relationships"]["0"]["songs"]);
-        setCurrentSongData(data["song"]);
-      })
-      .catch((err) => console.error(err));
+  const options: RequestInit = {
+    method: "GET",
+    headers: headers,
+    cache: "no-store",
+  };
 
-    console.log("hello");
-    console.log(currentSongID);
-  }, [currentSongID]);
+  return fetch(
+    "https://genius-song-lyrics1.p.rapidapi.com/album/appearances/?id=" +
+      id +
+      "&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => data["album_appearances"])
+    .catch((err) => console.error(err));
+}
+
+export default async function AlbumPage({ params }: any) {
+  const songs: any[] = await getAlbumAppearances(params.id);
+
+  const albumData: any = await getAlbumData(params.id);
 
   return (
-    <div>
-      <Link href={`/`}>
-        <h2
-          className={inter.className}
-          style={{ marginLeft: "1rem", marginBottom: "2rem" }}
-        >
-          Samplify
-        </h2>
-      </Link>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "end",
-        }}
-      >
-        {albumData != null && (
-          <div
-            style={{
-              display: "flex",
-              marginBottom: "2rem",
-              marginLeft: "1rem",
-            }}
-          >
+    <>
+      <div className={styles.description}>
+        <Link href={`/`}>
+          <p>
             <Image
-              src={albumData["cover_art_url"]}
-              alt={"Album Art"}
-              width={100}
-              height={100}
-              style={{
-                objectFit: "cover",
-                borderRadius: "5px",
-              }}
+              className={styles.logo}
+              src="/samplify.svg"
+              alt="Samplify X Logo"
+              width={150}
+              height={20}
+              priority
             />
-            <div style={{ paddingLeft: "1rem" }}>
-              <h1 className={inter.className}>{albumData["name"]}</h1>
-              {albumData["artist"]["name"] && (
-                <h2 className={inter.className}>
-                  {albumData["artist"]["name"]}
-                </h2>
-              )}
-            </div>
-          </div>
-        )}
-        {currentSongData && (
-          <p className={styles.card}>
-            <Link
-              href={`/samples/${currentSongData["id"]}`}
-              className={inter.className}
-            >
-              {currentSongData["title"]} <span>{">"}</span>
-            </Link>
           </p>
-        )}
+        </Link>
       </div>
-      <div style={{ display: "flex" }}>
-        <ul
-          style={{
-            height: "500px",
-            overflowY: "scroll",
-          }}
-        >
-          {songs?.map((song: any, index) => {
-            return (
-              <div
-                key={song["song"].id}
-                onClick={handleSongSelect.bind(handleSongSelect, index)}
-              >
-                <AlbumAppearanceCard
-                  clicked={song["song"].id == currentSongID}
-                  result={
-                    new AlbumAppearance(
-                      song["song"].id,
-                      song["song"]["title"],
-                      song["song"]["artist_names"]
-                    )
-                  }
-                />
-              </div>
-            );
-          })}
-        </ul>
+      {albumData && (
         <div
-          className={styles.grid}
           style={{
-            marginLeft: "1rem",
+            display: "flex",
+            margin: "1rem",
           }}
         >
-          {currentSongID && currentSamples.length == 0 ? (
-            <>
-              <h1 className={inter.className} style={{ margin: "1rem" }}>
-                ...
-              </h1>
-            </>
-          ) : (
-            currentSamples?.map((sample) => {
-              return (
-                <SampleResult
-                  key={sample.id}
-                  type="samples"
-                  sample={
-                    new Sample(
-                      sample.id,
-                      sample["title"],
-                      sample["artist_names"],
-                      sample["release_date_components"]
-                        ? sample["release_date_components"]["year"]
-                        : "-",
-                      sample["song_art_image_thumbnail_url"]
-                    )
-                  }
-                />
-              );
-            })
-          )}
+          <Image
+            src={albumData["cover_art_url"]}
+            alt={"Album Art"}
+            width={100}
+            height={100}
+            style={{
+              objectFit: "cover",
+              borderRadius: "5px",
+            }}
+          />
+          <div style={{ paddingLeft: "1rem" }}>
+            <h1 className={inter.className}>{albumData["name"]}</h1>
+            {albumData["artist"]["name"] && (
+              <h2 className={inter.className}>{albumData["artist"]["name"]}</h2>
+            )}
+          </div>
         </div>
+      )}
+      <div>
+        {songs?.map((song) => {
+          return (
+            <AlbumAppearanceResult
+              key={song.id}
+              songData={song}
+            />
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 }
