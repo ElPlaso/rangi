dc4bb2e1ca1ff4427efb3add1410278e9b55350f
kusaymoo <plahsoekusay@gmail.com>
2023-05-21 22:50:24 +1200

    search: replace text for clearing with inner x.
    replace the text button for clearing results,
    with a circular x button inside the search input,
    which appears when typed.
    this makes the results look better,
    as there is less unneeded text.
    update css selector to still allow
    results to appear.

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -1,103 +1,103 @@
-"use client";
+'use client'
 
-import React, { useState, useEffect, useRef } from "react";
-import "@/app/styles/search.css";
-import { Inter } from "@next/font/google";
-import SearchResult from "./search_result";
-import Result from "../models/result";
-import DotsLoader from "./dots_loader";
-import Link from "next/link";
+import React, { useState, useEffect, useRef } from 'react'
+import '@/app/styles/search.css'
+import { Inter } from '@next/font/google'
+import SearchResult from './search_result'
+import Result from '../models/result'
+import DotsLoader from './dots_loader'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Search(props: any) {
-  const [songResults, setSongResults] = useState([]);
-  const [albumResults, setAlbumResults] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState("");
+  const [songResults, setSongResults] = useState([])
+  const [albumResults, setAlbumResults] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [input, setInput] = useState('')
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const handleOnChange = (e: any) => {
-    setInput(e.target.value);
-  };
+    setInput(e.target.value)
+  }
 
   const clearResults = () => {
-    setSongResults([]);
-    setAlbumResults([]);
-    inputRef.current!.value = "";
-    setInput("");
-  };
+    setSongResults([])
+    setAlbumResults([])
+    inputRef.current!.value = ''
+    setInput('')
+  }
 
   // function to retry the request
   const retry = () => {
-    let headers = new Headers();
+    let headers = new Headers()
     headers.append(
-      "X-RapidAPI-Key",
+      'X-RapidAPI-Key',
       process.env.NEXT_PUBLIC_RAPID_API_KEY as string
-    );
-    headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
+    )
+    headers.append('X-RapidAPI-Host', 'genius-song-lyrics1.p.rapidapi.com')
     const options: RequestInit = {
-      method: "GET",
+      method: 'GET',
       headers: headers,
-      cache: "no-store",
-    };
-    setLoading(true);
+      cache: 'no-store',
+    }
+    setLoading(true)
     fetch(
-      "https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=" +
+      'https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=' +
         input +
-        "&per_page=3&page=1",
+        '&per_page=3&page=1',
       options
     )
       .then((response) => response.json())
       .then((data) => {
-        setSongResults(data.sections[1]["hits"]);
-        setAlbumResults(data.sections[4]["hits"]);
+        setSongResults(data.sections[1]['hits'])
+        setAlbumResults(data.sections[4]['hits'])
       })
       .then(() => setLoading(false))
-      .catch((err) => console.error(err));
-  };
+      .catch((err) => console.error(err))
+  }
 
   // set the songs when the input changes
   useEffect(() => {
     if (!input) {
-      setSongResults([]);
-      setAlbumResults([]);
-      return;
+      setSongResults([])
+      setAlbumResults([])
+      return
     }
-    let cancel = false;
-    setLoading(true);
-    let headers = new Headers();
+    let cancel = false
+    setLoading(true)
+    let headers = new Headers()
     headers.append(
-      "X-RapidAPI-Key",
+      'X-RapidAPI-Key',
       process.env.NEXT_PUBLIC_RAPID_API_KEY as string
-    );
-    headers.append("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com");
+    )
+    headers.append('X-RapidAPI-Host', 'genius-song-lyrics1.p.rapidapi.com')
     const options: RequestInit = {
-      method: "GET",
+      method: 'GET',
       headers: headers,
-      cache: "no-store",
-    };
-    setLoading(true);
+      cache: 'no-store',
+    }
+    setLoading(true)
     fetch(
-      "https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=" +
+      'https://genius-song-lyrics1.p.rapidapi.com/search/multi/?q=' +
         input +
-        "&per_page=3&page=1",
+        '&per_page=3&page=1',
       options
     )
       .then((response) => response.json())
       .then((data) => {
-        if (cancel) return;
-        setSongResults(data.sections[1]["hits"]);
-        setAlbumResults(data.sections[4]["hits"]);
+        if (cancel) return
+        setSongResults(data.sections[1]['hits'])
+        setAlbumResults(data.sections[4]['hits'])
       })
       .then(() => setLoading(false))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
 
     return () => {
-      cancel = true;
-    };
-  }, [input]);
+      cancel = true
+    }
+  }, [input])
 
   return (
     <div className="search">
@@ -107,40 +107,21 @@ export default function Search(props: any) {
         onChange={handleOnChange}
         placeholder="Search songs & albums"
       />
+      {input && (
+        <button className="clear-button" onClick={clearResults}></button>
+      )}
       <div className="search-results">
         {loading ? (
-          <div style={{ textAlign: "center" }}>
+          <div style={{ textAlign: 'center' }}>
             <DotsLoader />
           </div>
         ) : songResults.length > 0 || albumResults.length > 0 ? (
           <>
             <div
               style={{
-                marginTop: "0.5rem",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <h2 className={inter.className}>Results</h2>
-
-              <h4
-                onClick={clearResults}
-                className={inter.className}
-                style={{
-                  textDecoration: "underline",
-                  marginRight: "0.5rem",
-                  cursor: "pointer",
-                }}
-              >
-                Clear
-              </h4>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
               }}
             >
               <h3 className={inter.className}>Songs</h3>
@@ -148,7 +129,7 @@ export default function Search(props: any) {
                 <Link
                   href={`/search/${input}`}
                   className={inter.className}
-                  style={{ marginRight: "1rem", textDecoration: "underline" }}
+                  style={{ marginRight: '1rem', textDecoration: 'underline' }}
                 >
                   More
                 </Link>
@@ -156,19 +137,19 @@ export default function Search(props: any) {
             </div>
             {songResults.length > 0 &&
               songResults.map((result: any) => (
-                <div key={result["result"].id} onClick={props.onResultClick}>
+                <div key={result['result'].id} onClick={props.onResultClick}>
                   <SearchResult
                     type="samples"
-                    key={result["result"].id}
+                    key={result['result'].id}
                     result={
                       new Result(
-                        result["result"].id,
-                        result["result"]["title"],
-                        result["result"]["artist_names"],
-                        result["result"]["release_date_components"]
-                          ? result["result"]["release_date_components"]["year"]
-                          : "-",
-                        result["result"]["song_art_image_thumbnail_url"]
+                        result['result'].id,
+                        result['result']['title'],
+                        result['result']['artist_names'],
+                        result['result']['release_date_components']
+                          ? result['result']['release_date_components']['year']
+                          : '-',
+                        result['result']['song_art_image_thumbnail_url']
                       )
                     }
                   />
@@ -177,19 +158,19 @@ export default function Search(props: any) {
             <h3 className={inter.className}>Albums</h3>
             {albumResults.length > 0 &&
               albumResults.map((result: any) => (
-                <div key={result["result"].id} onClick={props.onResultClick}>
+                <div key={result['result'].id} onClick={props.onResultClick}>
                   <SearchResult
                     type="album"
-                    key={result["result"].id}
+                    key={result['result'].id}
                     result={
                       new Result(
-                        result["result"].id,
-                        result["result"]["name"],
-                        result["result"]["artist"]["name"],
-                        result["result"]["release_date_components"]
-                          ? result["result"]["release_date_components"]["year"]
-                          : "-",
-                        result["result"]["cover_art_url"]
+                        result['result'].id,
+                        result['result']['name'],
+                        result['result']['artist']['name'],
+                        result['result']['release_date_components']
+                          ? result['result']['release_date_components']['year']
+                          : '-',
+                        result['result']['cover_art_url']
                       )
                     }
                   />
@@ -199,12 +180,12 @@ export default function Search(props: any) {
         ) : (
           input.trim().length > 0 && (
             <div className="search-error">
-              <p className={inter.className} style={{ display: "flex" }}>
+              <p className={inter.className} style={{ display: 'flex' }}>
                 A problem occurred, please&nbsp;
                 <h4
                   style={{
-                    textDecoration: "underline",
-                    cursor: "pointer",
+                    textDecoration: 'underline',
+                    cursor: 'pointer',
                   }}
                   onClick={retry}
                 >
@@ -216,5 +197,5 @@ export default function Search(props: any) {
         )}
       </div>
     </div>
-  );
+  )
 }
