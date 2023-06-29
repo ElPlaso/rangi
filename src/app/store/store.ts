91667dfdc432a547ca4a62b5552d16f08fed5a8f
kusaymoo <plahsoekusay@gmail.com>
2023-06-30 02:42:30 +1200

    starred: save starred items to local storage.

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,12 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
-import starredSlice from "@/app/features/starred/starred-slice";
+import starredReducer, { setItems } from "@/app/features/starred/starred-slice";
 
 export const store = configureStore({
   reducer: {
-    starred: starredSlice,
+    starred: starredReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      (store) => (next) => (action) => {
+        let result = next(action);
+
+        // update local storage after reducer has handled action
+        if (
+          [
+            "starred/setItems",
+            "starred/addItem",
+            "starred/removeItem",
+          ].includes(action.type)
+        ) {
+          localStorage.setItem(
+            "starredItems",
+            JSON.stringify(store.getState().starred.items)
+          );
+        }
+
+        return result;
+      }
+    ),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+// Dispatch an action to initialize state from local storage after store creation
+if (typeof window !== "undefined") {
+  const localStorageStarredItems = localStorage.getItem("starredItems");
+  if (localStorageStarredItems) {
+    store.dispatch(setItems(JSON.parse(localStorageStarredItems)));
+  }
+}
 
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
