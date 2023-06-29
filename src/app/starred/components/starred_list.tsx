72c196a24fa86fc369a05eb65e7d92ebdbaa40b7
kusaymoo <plahsoekusay@gmail.com>
2023-06-30 04:34:50 +1200

    starred: display text when items are empty.

--- src/app/starred/components/starred_list.tsx
+++ src/app/starred/components/starred_list.tsx
@@ -3,11 +3,23 @@
 import { RootState } from "@/app/store/store";
 import { useSelector } from "react-redux";
 import styles from "@/app/styles/page.module.css";
+import "@/app/styles/styles.css";
 import StarredItem from "./starred_item";
 import FlipMove from "react-flip-move";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function StarredList() {
   const starred = useSelector((state: RootState) => state.starred.items);
+
+  if (starred.length === 0) {
+    return (
+      <div className="cardish hovered">
+        <p className={inter.className}>You do not have any starred samples.</p>
+      </div>
+    );
+  }
 
   return (
     <FlipMove
