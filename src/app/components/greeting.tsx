1ee8d3deb4a8ab5f6e5c8a8ccec4a427eb339db4
Gareth McIntosh <mcintogare@le-metropolitain.ecs.vuw.ac.nz>
2023-07-16 18:04:22 +1200

    added check to display afternoon

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -12,8 +12,10 @@ export default function Greeting() {
   return (
     <h1 className="font-semibold lg:text-3xl md:text-2xl sm:text-lg max-sm:text-lg">
       Good{" "}
-      {time >= 6 && time < 18 ? (
+      {time >= 6 && time < 12 ? (
         <>morning</>
+      ) : time >= 12 && time < 18 ? (
+        <>afternoon</>
       ) : time >= 18 && time < 24 ? (
         <>evening</>
       ) : (
