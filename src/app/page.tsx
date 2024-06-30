75664fcc59debdb051618d7bb10e856f7445df3c
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2024-06-30 21:37:16 +1200

    Update branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,50 +7,28 @@ import InfoCard from "@/components/home/info_card";
 
 export default function Home() {
   return (
-    <>
-      <div className="container flex flex-col">
-        <div className="w-full">
-          <Greeting />
-        </div>
-        <div className="w-full">
-          <Splash />
-          <div className="mb-8 md:grid md:grid-cols-3 md:gap-x-3 sm:flex sm:flex-col md:space-y-0 sm:space-y-2 max-sm:space-y-2">
-            <InfoCard
-              title="Discover"
-              description="Discover new music through samples used in your favourite songs and albums."
-            />
-            <InfoCard
-              title="Compare"
-              description="Compare songs to their samples and the songs that sample them."
-            />
-            <InfoCard
-              title="Explore"
-              description="Explore the nearly endless knot of music and samples."
-            />
-          </div>
-          <Links />
-        </div>
+    <div className="container flex flex-col">
+      <div className="w-full">
+        <Greeting />
       </div>
-      <div className="fixed bottom-0 flex items-center justify-center w-full p-5 mt-24 lg:bg-transparent md:dark:bg-black sm:dark:bg-black max-sm:dark:bg-black backdrop-blur-2xl">
-        <div className="flex flex-row items-center justify-center space-x-2">
-          <Image
-            src={"/favicon.ico"}
-            alt="BeatBotanica Logo"
-            width={28}
-            height={28}
+      <div className="w-full">
+        <Splash />
+        <div className="mb-8 md:grid md:grid-cols-3 md:gap-x-3 sm:flex sm:flex-col md:space-y-0 sm:space-y-2 max-sm:space-y-2">
+          <InfoCard
+            title="Discover"
+            description="Discover new music through samples used in your favourite songs and albums."
           />
-          <div>
-            Brought to you by{" "}
-            <a
-              href="https://beatbotanica.com"
-              className="text-blue-600 hover:underline"
-            >
-              {" "}
-              beatbotanica.com
-            </a>
-          </div>
+          <InfoCard
+            title="Compare"
+            description="Compare songs to their samples and the songs that sample them."
+          />
+          <InfoCard
+            title="Explore"
+            description="Explore the nearly endless knot of music and samples."
+          />
         </div>
+        <Links />
       </div>
-    </>
+    </div>
   );
 }
